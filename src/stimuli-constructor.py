68333307ce92a7c7f2571b5955447c921c79a7dd
Sanghee J. Kim <sangheekim@Sanghees-MacBook-Air.local>
2025-02-07 16:26:18 -0600

    stimuli generation for manual data

--- src/stimuli-constructor.py
+++ src/stimuli-constructor.py
@@ -13,39 +13,41 @@ Script to generate and save stimuli. Multiple modes:
 import argparse
 import pathlib
 import utils
+import pandas as pd
 
 from string import Template
 
 
+# ----------------------------------------------------
+
 TEMPLATE_MANUAL = Template(
-    '$name1 said, "$subj, who $arc, $mc." $name2 replied, "$generated_sent."' # TODO: strip '.' from $generated_set
+    '$name1 said, "$subj, who $arc, $mc." $name2 replied, "$cont_sentence"'
 )
 
-def generate_manual_stimuli(name1, subj, vps, name2, vp_conts):
-    continuation_arc = TEMPLATE_MANUAL.substitute(
+def generate_manual_stimuli(name1, name2, subj, vp1, vp2, continuation):
+    
+    continuation = continuation.strip('.')
+    continuation = continuation + '.'
+    if continuation.endswith(('?.', '!.')):
+        continuation = continuation[:-1]
+
+    manually_generated_dialogue = TEMPLATE_MANUAL.substitute(
         name1 = name1, 
         subj = subj,
-        arc = vps[0],
-        mc = vps[1],
+        arc = vp1,
+        mc = vp2,
         name2 = name2,
-        generated_sent = vp_conts[0],
-    )
-    continuation_mc = TEMPLATE_MANUAL.substitute(
-        name1 = name1, 
-        subj = subj,
-        arc = vps[1],
-        mc = vps[0],
-        name2 = name2,
-        generated_sent = vp_conts[1],
+        cont_sentence = continuation
     )
 
-    return continuation_arc, continuation_mc
+    return manually_generated_dialogue
+    
+# ----------------------------------------------------
 
 
 TEMPLATE = Template(
     '$name1 said, "$subj, who $arc, $mc." $name2 replied, "$negation,[SPLIT]$prn $mainverb not."'
 )
-
 
 def generate_kim22_stimuli(name1, subj, vps, name2, prn, verbs):
     """vps formatted as vp1, vp2"""
@@ -101,7 +103,7 @@ def main(args):
 
     eval_data = []
     if mode == "kim22":
-        # kim, lang, ettinger (2022)
+        # kim, yu, ettinger (2022)
 
         eval_items = utils.read_csv_dict(item_path)
         idx = 0
@@ -220,8 +222,35 @@ def main(args):
         
 
     elif mode == "manual":
-        """For the stimuli that Sanghee creates manually!"""
-        pass
+        # using manually generated code
+
+        eval_items = pd.read_csv(item_path)
+        new_idx = 0
+        for i, row in eval_items.iterrows():
+            generated_manual_stimulus = generate_manual_stimuli(
+                row['name1'],
+                row['name2'],
+                row['subj'],
+                row['vp1'],
+                row['vp2'],
+                row['continuation']
+                )
+            eval_data.append(
+                {
+                    "new_idx": new_idx,
+                    "idx": row['idx'],
+                    "cont_type": row['continuation_type'],
+                    "cont_target": "arc" if row['vp'] == 1 else "mc" if row['vp'] == 2 else None,
+                    "cont_id": row['continuation_id'],
+                    "stimuli": generated_manual_stimulus
+                }
+            )
+            new_idx += 1
+            # breakpoint()
+        # pass
+
+        pathlib.Path(save_dir).mkdir(parents = True, exist_ok = True)
+        utils.write_dict_list_to_csv(eval_data, f"{save_dir}/manual-full.csv")
     else:
         raise NotImplementedError
 
@@ -229,9 +258,14 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--mode", type=str, default="kim22")
-    parser.add_argument("--item-path", type=str, default="data/used_items.csv")
-    parser.add_argument("--save-dir", type=str, default="data/stimuli")
+    # parser.add_argument("--mode", type=str, default="kim22")
+    # parser.add_argument("--item-path", type=str, default="../data/used_items.csv")
+    # parser.add_argument("--save-dir", type=str, default="../data/stimuli")
+
+    parser.add_argument("--mode", type=str, default="manual")
+    parser.add_argument("--item-path", type=str, default="../data/manual-items.csv")
+    parser.add_argument("--save-dir", type=str, default="../data/stimuli")
 
     args = parser.parse_args()
+
     main(args)
