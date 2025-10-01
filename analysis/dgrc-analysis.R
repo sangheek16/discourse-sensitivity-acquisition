15f17b5b5d1b550ab6a7812ba47a7d0dcedb1920
Kanishka Misra <menogetusername@gmail.com>
2025-09-30 23:10:27 -0500

    modelling and plots

--- analysis/dgrc-analysis.R
+++ analysis/dgrc-analysis.R
@@ -1,24 +1,26 @@
 library(tidyverse)
 library(lmerTest)
 library(broom.mixed)
+library(emmeans)
 
 model_meta <- tribble(
-  ~model, ~short, ~class, ~instruct, ~params,
-  "llama-3-8b", "L-3-8B", "Llama-3-8B", FALSE, 8000000000,
-  "llama-3-8b-instruct", "L-3-8B-I", "Llama-3-8B", TRUE, 8000000000,
-  "qwen2.5-500m", "Q-2.5-500M", "Qwen2.5", FALSE, 500000000,
-  "qwen2.5-500m-instruct", "Q-2.5-500M-I", "Qwen2.5", TRUE, 500000000,
-  "qwen2.5-1.5b", "Q-2.5-1.5B", "Qwen2.5", FALSE, 1500000000,
-  "qwen2.5-1.5b-instruct", "Q-2.5-1.5B-I", "Qwen2.5", TRUE, 1500000000,
-  "qwen2.5-3b", "Q-2.5-3B", "Qwen2.5", FALSE, 3000000000,
-  "qwen2.5-3b-instruct", "Q-2.5-3B-I", "Qwen2.5", TRUE, 3000000000,
-  "qwen2.5-7b", "Q-2.5-7B", "Qwen2.5", FALSE, 7000000000,
-  "qwen2.5-7b-instruct", "Q-2.5-7B-I", "Qwen2.5", TRUE, 7000000000,
+  ~model, ~short, ~base, ~class, ~instruct, ~params,
+  "llama-3-8b", "L-3-8B", "llama-3-8b", "Llama-3-8B", FALSE, 8000000000,
+  "llama-3-8b-instruct", "L-3-8B-I", "llama-3-8b", "Llama-3-8B", TRUE, 8000000000,
+  "qwen2.5-500m", "Q-2.5-500M", "qwen-2.5-500M", "Qwen2.5", FALSE, 500000000,
+  "qwen2.5-500m-instruct", "Q-2.5-500M-I", "qwen-2.5-500M", "Qwen2.5", TRUE, 500000000,
+  "qwen2.5-1.5b", "Q-2.5-1.5B", "qwen-2.5-1.5B", "Qwen2.5", FALSE, 1500000000,
+  "qwen2.5-1.5b-instruct", "Q-2.5-1.5B-I", "qwen-2.5-1.5B", "Qwen2.5", TRUE, 1500000000,
+  "qwen2.5-3b", "Q-2.5-3B", "qwen-2.5-3B", "Qwen2.5", FALSE, 3000000000,
+  "qwen2.5-3b-instruct", "Q-2.5-3B-I", "qwen-2.5-3B", "Qwen2.5", TRUE, 3000000000,
+  "qwen2.5-7b", "Q-2.5-7B", "qwen-2.5-7B", "Qwen2.5", FALSE, 7000000000,
+  "qwen2.5-7b-instruct", "Q-2.5-7B-I", "qwen-2.5-7B","Qwen2.5", TRUE, 7000000000,
 ) %>%
   mutate(
     short = factor(short, levels = c("L-3-8B", "L-3-8B-I", "Q-2.5-500M", "Q-2.5-500M-I", 
                                      "Q-2.5-1.5B", "Q-2.5-1.5B-I", "Q-2.5-3B", "Q-2.5-3B-I", 
                                      "Q-2.5-7B", "Q-2.5-7B-I")),
+    base = factor(base, levels = c("llama-3-8b", "qwen-2.5-500M", "qwen-2.5-1.5B", "qwen-2.5-3B", "qwen-2.5-7B"))
   )
 
 stimuli <- fs::dir_ls("data/results/sorted-generations/freeform/", regexp = "*.csv") %>%
@@ -89,23 +91,92 @@ metric <- nested %>%
     )
   ) %>%
   select(-data) %>%
-  inner_join(model_meta %>% select(model, class, instruct))
+  inner_join(model_meta %>% select(model, base, instruct))
 
-fit1 <- lmer(recency ~ mode + swapped + instruct + (1 + mode + swapped + instruct || class) + (1 + mode + swapped + instruct || item), data = metric, REML = FALSE)
-fit1.swapped <- lmer(recency ~ mode + instruct + (1 + mode + swapped + instruct || class) + (1 + mode + swapped + instruct || item), data = metric, REML = FALSE)
-fit1.mode <- lmer(recency ~ swapped + instruct + (1 + mode + swapped + instruct || class) + (1 + mode + swapped + instruct || item), data = metric, REML = FALSE)
-fit1.instruct <- lmer(recency ~ mode + swapped + (1 + mode + swapped || class) + (1 + mode + swapped || item), data = metric, REML = FALSE)
+metric_reg <- metric %>%
+  mutate(
+    mode = case_when(
+      mode == "coord" ~ -0.5,
+      TRUE ~ 0.5
+    ),
+    instruct = case_when(
+      instruct == TRUE ~ 0.5,
+      TRUE ~ -0.5
+    ),
+    class = base
+  )
+
+fit1 <- lmer(recency ~ swapped + instruct * mode + (1 + swapped + mode | class ) + (1 + swapped + mode * instruct | item), data = metric_reg, REML=FALSE)
+fit1.swapped <- lmer(recency ~ instruct * mode + (1 + swapped + mode | class ) + (1 + swapped + mode * instruct | item), data = metric_reg, REML=FALSE)
+fit1.mode <- lmer(recency ~ swapped + instruct + instruct:mode + (1 + swapped + mode | class ) + (1 + swapped + mode * instruct | item), data = metric_reg, REML=FALSE)
+fit1.instruct <- lmer(recency ~ swapped + instruct:mode + mode + (1 + swapped + mode | class ) + (1 + swapped + mode * instruct | item), data = metric_reg, REML=FALSE)
 
 anova(fit1, fit1.swapped)
 anova(fit1, fit1.mode)
 anova(fit1, fit1.instruct)
 
-fit1.full <- lmer(recency ~ swapped + instruct * mode + (1 + swapped + instruct * mode || class) + (1 + swapped + instruct * mode || item), data = metric, REML = TRUE)
+# fit1.full <- lmer(recency ~ swapped + instruct * mode + (1 + swapped + mode | class ) + (1 + swapped + mode + instruct | item), data = metric_reg)
+# summary(fit1.full)
 
+fit1.full <- lmer(recency ~ swapped + instruct * mode + (1 | class) + (1 + swapped + instruct * mode | item), data = metric_reg)
 summary(fit1.full)
+
+emmip(fit1.full, instruct ~ mode, CIs = TRUE, plotit = FALSE) %>%
+  as_tibble() %>%
+  mutate(
+    instruct = case_when(
+      instruct == -0.5 ~ FALSE,
+      TRUE ~ TRUE
+    ),
+    mode = case_when(
+      mode == -0.5 ~ "Coord",
+      TRUE ~ "ARC"
+    )
+  ) %>%
+  ggplot(aes(mode, yvar, color = instruct, linetype = instruct, shape = instruct)) +
+  geom_point(size = 2.5) +
+  geom_line(aes(group = instruct)) +
+  geom_linerange(linetype = "solid", aes(ymin = yvar-SE, ymax=yvar+SE)) +
+  scale_color_brewer(palette = "Dark2") +
+  scale_y_continuous(labels = scales::percent_format(), limits = c(0.4,0.7)) +
+  theme_bw(base_size = 16, base_family = "Times") +
+  theme(
+    axis.text = element_text(color = "black"),
+    panel.grid = element_blank(),
+    legend.position = "top"
+  ) +
+  labs(
+    x = "Structure",
+    y = "Estimated VP2-Preference",
+    color = "Instruct",
+    fill = "Instruct",
+    shape = "Instruct",
+    linetype = "Instruct"
+  )
+
+ggsave("plots/structure-instruct-interaction-main-exp.pdf", height = 4.04, width = 4.44, dpi = 400, device=cairo_pdf)
+
+emmeans(fit1.full, ~ instruct * mode) %>%
+  pairs() %>%
+  as_tibble() %>%
+  mutate(
+    contrast = str_replace(contrast, "(instruct-0.5 mode-0.5)", "base + coord"),
+    contrast = str_replace(contrast, "(instruct-0.5 mode0.5)", "base + arc"),
+    contrast = str_replace(contrast, "(instruct0.5 mode0.5)", "instruct + arc"),
+    contrast = str_replace(contrast, "(instruct0.5 mode-0.5)", "instruct + coord"),
+  )
+
+emmeans(fit1.full, ~ instruct * mode) %>%
+  contrast(simple = "each")
+
+emmeans(fit1.full, pairwise ~ mode : instruct)
+
 
 tidy(fit1.full) %>%
   filter(effect == "fixed")
+
+
+
 
 metric %>%
   group_by(model, mode, swapped) %>%
@@ -117,6 +188,12 @@ metric %>%
   ) %>%
   ungroup() %>%
   inner_join(model_meta) %>%
+  mutate(
+    mode = case_when(
+      mode == "arc" ~ "ARC",
+      TRUE ~ "Coord"
+    )
+  ) %>%
   # filter(swapped == FALSE) %>%
   ggplot(aes(params/1e9, mean, color = instruct, fill = instruct, shape = class, linetype = swapped)) +
   geom_point(size = 2.5) +
@@ -124,20 +201,26 @@ metric %>%
   # geom_ribbon(aes(ymin = mean-cb, ymax = mean+cb), color = NA, alpha = 0.2) +
   geom_linerange(aes(ymin = mean-cb, ymax = mean+cb), linetype = "solid", linewidth = 0.3) +
   geom_hline(yintercept = 0.5, linetype = "dashed") +
-  scale_y_continuous(limits = c(0,1), labels = scales::percent_format()) +
+  scale_y_continuous(limits = c(0.3,0.8), labels = scales::percent_format()) +
   scale_x_log10(limits = c(0.5, 8), breaks = c(0.5,1,2,4,6,8), labels = c("1/2", "1", "2", "4", "6", "8")) +
   scale_color_brewer(palette = "Dark2", aesthetics = c("color", "fill")) +
   facet_wrap(~mode) +
-  theme_bw(base_size = 16) +
+  theme_bw(base_size = 16, base_family = "Times") +
   theme(
     axis.text = element_text(color = "black")
   ) +
   labs(
     x = "Parameters (in billion)",
-    y = "VP2-preference"
+    y = "VP2-preference",
+    color = "Instruct",
+    fill = "Instruct",
+    shape = "Model",
+    linetype = "Swapped"
   )
 
-ggsave("plots/exp1-dgrc.pdf", width = 7.68, height = 4.92, dpi=300, device=cairo_pdf)
+# ggsave("plots/dgrc-main-exp.pdf", width = 8, height = 4.92, dpi=300, device=cairo_pdf)
+ggsave("plots/dgrc-main-exp.pdf", width = 7.14, height = 3.60, dpi=300, device=cairo_pdf)
+
 
 # systematicity test
 
--- analysis/dgrc-rejection-analysis.R
+++ analysis/dgrc-rejection-analysis.R
@@ -2,22 +2,23 @@ library(tidyverse)
 library(lmerTest)
 
 model_meta <- tribble(
-  ~model, ~short, ~class, ~instruct, ~params,
-  "llama-3-8b", "L-3-8B", "Llama-3-8B", FALSE, 8000000000,
-  "llama-3-8b-instruct", "L-3-8B-I", "Llama-3-8B", TRUE, 8000000000,
-  "qwen2.5-500m", "Q-2.5-500M", "Qwen2.5", FALSE, 500000000,
-  "qwen2.5-500m-instruct", "Q-2.5-500M-I", "Qwen2.5", TRUE, 500000000,
-  "qwen2.5-1.5b", "Q-2.5-1.5B", "Qwen2.5", FALSE, 1500000000,
-  "qwen2.5-1.5b-instruct", "Q-2.5-1.5B-I", "Qwen2.5", TRUE, 1500000000,
-  "qwen2.5-3b", "Q-2.5-3B", "Qwen2.5", FALSE, 3000000000,
-  "qwen2.5-3b-instruct", "Q-2.5-3B-I", "Qwen2.5", TRUE, 3000000000,
-  "qwen2.5-7b", "Q-2.5-7B", "Qwen2.5", FALSE, 7000000000,
-  "qwen2.5-7b-instruct", "Q-2.5-7B-I", "Qwen2.5", TRUE, 7000000000,
+  ~model, ~short, ~base, ~class, ~instruct, ~params,
+  "llama-3-8b", "L-3-8B", "llama-3-8b", "Llama-3-8B", FALSE, 8000000000,
+  "llama-3-8b-instruct", "L-3-8B-I", "llama-3-8b", "Llama-3-8B", TRUE, 8000000000,
+  "qwen2.5-500m", "Q-2.5-500M", "qwen-2.5-500M", "Qwen2.5", FALSE, 500000000,
+  "qwen2.5-500m-instruct", "Q-2.5-500M-I", "qwen-2.5-500M", "Qwen2.5", TRUE, 500000000,
+  "qwen2.5-1.5b", "Q-2.5-1.5B", "qwen-2.5-1.5B", "Qwen2.5", FALSE, 1500000000,
+  "qwen2.5-1.5b-instruct", "Q-2.5-1.5B-I", "qwen-2.5-1.5B", "Qwen2.5", TRUE, 1500000000,
+  "qwen2.5-3b", "Q-2.5-3B", "qwen-2.5-3B", "Qwen2.5", FALSE, 3000000000,
+  "qwen2.5-3b-instruct", "Q-2.5-3B-I", "qwen-2.5-3B", "Qwen2.5", TRUE, 3000000000,
+  "qwen2.5-7b", "Q-2.5-7B", "qwen-2.5-7B", "Qwen2.5", FALSE, 7000000000,
+  "qwen2.5-7b-instruct", "Q-2.5-7B-I", "qwen-2.5-7B","Qwen2.5", TRUE, 7000000000,
 ) %>%
   mutate(
     short = factor(short, levels = c("L-3-8B", "L-3-8B-I", "Q-2.5-500M", "Q-2.5-500M-I", 
                                      "Q-2.5-1.5B", "Q-2.5-1.5B-I", "Q-2.5-3B", "Q-2.5-3B-I", 
                                      "Q-2.5-7B", "Q-2.5-7B-I")),
+    base = factor(base, levels = c("llama-3-8b", "qwen-2.5-500M", "qwen-2.5-1.5B", "qwen-2.5-3B", "qwen-2.5-7B"))
   )
 
 stimuli <- fs::dir_ls("data/results/sorted-generations/rejection/", regexp = "*.csv") %>%
@@ -97,10 +98,6 @@ metric <- nested %>%
 
 metric
 
-fit_reject <- lmer(recency ~ header * mode + swapped + (1 + header * mode + swapped | model) + (1 + header * mode + swapped | item), data = metric)
-
-summary(fit_reject)
-
 metric %>%
   group_by(model, mode, swapped, header) %>%
   summarize(
@@ -110,27 +107,145 @@ metric %>%
     mean = mean(recency)
   ) %>%
   ungroup() %>%
+  filter(swapped == FALSE) %>%
   inner_join(model_meta) %>%
-  ggplot(aes(params/1e9, mean, color = instruct, fill = instruct, shape = class, linetype = swapped)) +
+  mutate(
+    mode = case_when(
+      mode == "arc" ~ "ARC",
+      TRUE ~ "Coord"
+    ),
+    header = case_when(
+      header == "no" ~ "Absent",
+      TRUE ~ "Present"
+    )
+  ) %>%
+  ggplot(aes(params/1e9, mean, color = header, fill = header, shape = class, linetype = instruct)) +
   geom_point(size = 2.5) +
   geom_line() +
   # geom_ribbon(aes(ymin = mean-cb, ymax = mean+cb), color = NA, alpha = 0.2) +
   geom_linerange(aes(ymin = mean-cb, ymax = mean+cb), linetype = "solid", linewidth = 0.3) +
   geom_hline(yintercept = 0.5, linetype = "dashed") +
-  scale_y_continuous(limits = c(0,1), labels = scales::percent_format()) +
+  scale_y_continuous(limits = c(0.3,0.7), labels = scales::percent_format()) +
   scale_x_log10(limits = c(0.5, 8), breaks = c(0.5,1,2,4,6,8), labels = c("1/2", "1", "2", "4", "6", "8")) +
-  scale_color_brewer(palette = "Dark2", aesthetics = c("color", "fill")) +
-  facet_grid(header ~ mode) +
-  theme_bw(base_size = 16) +
+  # scale_color_brewer(palette = "Dark2", aesthetics = c("color", "fill")) +
+  scale_color_manual(values = c("#7570b3", "#e6ab02"), aesthetics = c("color", "fill")) +
+  # facet_grid(header ~ mode) +
+  facet_wrap(~mode) +
+  theme_bw(base_size = 16, base_family = "Times") +
   theme(
     axis.text = element_text(color = "black")
   ) +
   labs(
     x = "Parameters (in billion)",
-    y = "VP2-preference"
+    y = "VP2-preference",
+    linetype = "Instruct",
+    shape = "Model",
+    color = "Digression Signal",
+    fill = "Digression Signal"
   )
 
-ggsave("plots/exp3-dgrc-rejection.pdf", width = 7.68, height = 6.47, dpi = 300, device=cairo_pdf)
+ggsave("plots/dgrc-digression-exp.pdf", width = 7.14, height = 3.60, dpi=300, device=cairo_pdf)
+
+
+metric_reg <- metric %>%
+  filter(swapped == FALSE) %>%
+  inner_join(model_meta) %>%
+  mutate(
+    header = case_when(
+      header == "no" ~ 0.5,
+      TRUE ~ -0.5
+    ),
+    mode = case_when(
+      mode == "coord" ~ -0.5,
+      TRUE ~ 0.5
+    ),
+    instruct = case_when(
+      instruct == TRUE ~ 0.5,
+      TRUE ~ -0.5
+    )
+  )
+
+fit2 <- lmer(recency ~ header * instruct * mode + (1 + header + instruct + mode | base) + (1 + header + instruct + mode | item), data= metric_reg)
+summary(fit2)
+
+emmip(fit2, header ~ mode, CIs = TRUE, plotit = FALSE) %>%
+  as_tibble() %>%
+  mutate(
+    header = case_when(
+      header == -0.5 ~ "Present",
+      TRUE ~ "Absent"
+    ),
+    mode = case_when(
+      mode == -0.5 ~ "Coord",
+      TRUE ~ "ARC"
+    )
+  ) %>%
+  ggplot(aes(mode, yvar, color = header, linetype = header, shape = header)) +
+  geom_point(size = 2.5) +
+  geom_line(aes(group = header)) +
+  geom_linerange(linetype = "solid", aes(ymin = yvar-SE, ymax=yvar+SE)) +
+  # scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = c("#7570b3", "#e6ab02")) +
+  scale_y_continuous(labels = scales::percent_format(), limits = c(0.4,0.7)) +
+  # guides(col = guide_legend(nrow = 2)) +
+  theme_bw(base_size = 15, base_family = "Times") +
+  theme(
+    axis.text = element_text(color = "black"),
+    panel.grid = element_blank(),
+    legend.position = "top",
+    legend.text = element_text(size = 12),
+    legend.title = element_text(size = 13)
+  ) +
+  labs(
+    x = "Structure",
+    y = "Estimated VP2-Preference",
+    color = "Digression Signal",
+    fill = "Digression Signal",
+    shape = "Digression Signal",
+    linetype = "Digression Signal"
+  )
+
+# ggsave("plots/structure-digression-interaction.pdf", height = 3.5, width = 3.86, dpi = 300, device=cairo_pdf)
+ggsave("plots/structure-digression-interaction.pdf", height = 4.04, width = 4.44, dpi = 400, device=cairo_pdf)
+
+
+emmip(fit1.full, instruct ~ mode, CIs = TRUE, plotit = FALSE) %>%
+  as_tibble() %>%
+  mutate(
+    instruct = case_when(
+      instruct == -0.5 ~ FALSE,
+      TRUE ~ TRUE
+    ),
+    mode = case_when(
+      mode == -0.5 ~ "Coord",
+      TRUE ~ "ARC"
+    )
+  ) %>%
+  ggplot(aes(mode, yvar, color = instruct, linetype = instruct, shape = instruct)) +
+  geom_point(size = 2.5) +
+  geom_line(aes(group = instruct)) +
+  geom_linerange(linetype = "solid", aes(ymin = yvar-SE, ymax=yvar+SE)) +
+  scale_color_brewer(palette = "Dark2") +
+  scale_y_continuous(labels = scales::percent_format(), limits = c(0.4,0.7)) +
+  theme_bw(base_size = 16, base_family = "Times") +
+  theme(
+    axis.text = element_text(color = "black"),
+    panel.grid = element_blank(),
+    legend.position = "top"
+  ) +
+  labs(
+    x = "Structure",
+    y = "Estimated VP2-Preference",
+    color = "Instruct",
+    fill = "Instruct",
+    shape = "Instruct",
+    linetype = "Instruct"
+  )
+
+ggsave("plots/structure-instruct-interaction-digression-exp.pdf", height = 4.04, width = 4.44, dpi = 400, device=cairo_pdf)
+
+
+# ggsave("plots/exp3-dgrc-rejection.pdf", width = 7.68, height = 6.47, dpi = 300, device=cairo_pdf)
 # 768 x 647
 
 ttested <- metric %>%
